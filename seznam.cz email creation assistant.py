2f20f55190e634bd05b90482c44ee90d64203d80
blakeando <blakeando@live.com>
2019-02-09 02:58:48 +1100

    release

---
+++ seznam.cz email creation assistant.py
@@ -0,0 +1,27 @@
+import uuid
+import pyperclip
+
+usehooks = 0
+
+while True:
+    rand = str(uuid.uuid4())
+    input("Press enter for random username")
+    pyperclip.copy(str(rand[:8]))
+    print (str(rand[:8]) + " " + "Has been copied to clipboard")
+    input("Press enter for random password")
+    pyperclip.copy(str(rand))
+    print (str(rand) + " " + "Has been copied to clipboard")
+    input("Press enter for year of birth")
+    print ("1990 Has been copied to clipboard")
+    pyperclip.copy("1990")
+    file = open("emails.txt","a")
+    file.write(str(rand[:8]) + "@seznam.cz" + ":" + str(rand)+ '\n')
+    file.close()
+
+    if usehooks == 1:
+        from dhooks import Webhook
+        hook = Webhook('WEBHOOK_HERE_IF_ENABLED')
+        hook.send("```New Seznam account```")
+        hook.send("```" + str(rand[:8]) + "@seznam.cz" + ":" + str(rand) + "```")
+        
+    
